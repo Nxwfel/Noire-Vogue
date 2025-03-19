a71090b6a10382fd12f764f1dde96aac681edba6
Nxwfel <nelxpe@gmail.com>
2025-03-19 04:51:54 +0000

    Added animated bg

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,6 +4,11 @@ const Header = () => {
   return (
     <div className='absolute pt-3 h-16 w-screen items-center justify-center '>
     <div className='relative mr-auto ml-auto rounded-full h-16 w-39 bg-neutral-950 inset-shadow-sm inset-shadow-neutral-700'>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </div>
     </div>
   )
