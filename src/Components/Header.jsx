44289b8531a7bc8826eefb799c900a732d3af2c5
Nxwfel <nelxpe@gmail.com>
2025-03-19 07:56:41 +0000

    Styling a bit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='absolute pt-3 h-16 w-screen items-center justify-center '>
-    <div className='relative mr-auto ml-auto rounded-full h-16 w-39 bg-neutral-950 inset-shadow-sm inset-shadow-neutral-700'>
-      <ul>
+    <div className='relative mr-auto ml-auto rounded-full h-16 w-60 bg-neutral-950 inset-shadow-sm inset-shadow-neutral-500'>
+      <ul className='flex justify-around items-center'>
         <li>Home</li>
         <li>About</li>
         <li>Contact</li>
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Background from '../Assets/background.mp4'
 import Header from './Header.jsx'
-import Spline from '@splinetool/react-spline';
 const HomePage = () => {
   return (
     <div className='h-screen w-screen overflow-hidden items-center justify-center'>
@@ -9,9 +8,10 @@ const HomePage = () => {
         <div> 
         <video src={Background} autoPlay loop muted  className='h-screen w-screen object-cover'/>
         </div>
-        <div className='absolute left-3'>
-          <h1 className='-mt-[35vw] text-8xl text-white font-mono '>Noire Vauge</h1>
-          <h3 className='text-2xl text-white font-mono '>Elegance a porter</h3>
+
+        <div className='absolute bottom-10 left-3 text-start'>
+          <h1 className='font-syne text-6xl text-white'>Noire Vauge</h1>
+          <h3 className='font-syne text-2xl text-neutral-600'>Elegance a porter</h3>
         </div>
     </div>
   )
