856016642aa08000f9b71cb76c2d3183fbec00ca
Nxwfel <nelxpe@gmail.com>
2025-03-25 02:28:23 +0000

    Style done remaining: Responssivnes + scrolling

--- src/Components/ProductPage.jsx
+++ src/Components/ProductPage.jsx
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom';
 const ProductPage = () => {
        const [quantity, setQuantity] = useState(1)
        const [price, setPrice] = useState(250)
-
        const Quantity_addition = () =>{
           setQuantity(quantity+1)
           setPrice(price+250)
@@ -29,9 +28,10 @@ const ProductPage = () => {
     className='h-screen w-screen bg-cover bg-center flex flex-col'
     style={{ backgroundImage: `url(${Bg3})` }}
     >
+    <div className='h-1/4 w-screen flex items-center '>
       <Link to={'/products'}>
     <button
-        class="bg-transparent ml-2 mr-auto mb-10 mt-10 text-center w-48 rounded-2xl h-14 relative text-white cursor-pointer text-xl font-syne font-semibold group"
+        class="bg-transparent ml-2 mr-auto mb-10 text-center w-48 rounded-2xl h-14 relative text-white cursor-pointer text-xl font-syne font-semibold group"
         type="button"
       >
         <div
@@ -56,6 +56,7 @@ const ProductPage = () => {
         <p class="translate-x-2">Go Back</p>
       </button>
       </Link>
+      </div>
      <div className='h-2/3 w-2/3 relative ml-auto mr-auto rounded-xl border-1 border-stone-400 bg-white/20 flex items-center justify-center text-justify'>
       <div id='Product-Pic' className='relative mt-auto mb-auto  mr-auto -ml-30 rotate-20 h-2/3 w-1/2'>
       <img src={Product} alt=""  className='-mt-40'/>
@@ -94,7 +95,38 @@ const ProductPage = () => {
            </div>
         </div>
         <div id='Product-price' className='h-1/3 w-full'><h1 id='Price' className='font-syne text-2xl font-bold text-black'>Price: <span className='text-white'>{price} $ </span> </h1></div>
-        
+        <button
+            class="cursor-pointer group relative bg-white hover:bg-zinc-300 text-black font-semibold text-sm px-2 py-3 rounded-full transition-all duration-200 ease-in-out shadow hover:shadow-lg w-40 h-12 mb-10"
+          >
+            <div class="relative flex items-center justify-center gap-2">
+              <span class="relative inline-block overflow-hidden">
+                <span
+                  class="block transition-transform duration-300 group-hover:-translate-y-full"
+                >
+                  Buy Now!
+                </span>
+                <span
+                  class="absolute inset-0 transition-transform duration-300 translate-y-full group-hover:translate-y-0"
+                >
+                  Thanks
+                </span>
+              </span>
+
+              <svg
+                class="w-4 h-4 transition-transform duration-200 group-hover:rotate-45"
+                viewBox="0 0 24 24"
+              >
+                <circle fill="currentColor" r="11" cy="12" cx="12"></circle>
+                <path
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  stroke-width="2"
+                  stroke="white"
+                  d="M7.5 16.5L16.5 7.5M16.5 7.5H10.5M16.5 7.5V13.5"
+                ></path>
+              </svg>
+            </div>
+          </button>
       </div>
      </div>
     </div>
