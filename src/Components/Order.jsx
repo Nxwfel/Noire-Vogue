5b7a5aa49997d820e27019a00d9c8cc08334feb0
Nxwfel <nelxpe@gmail.com>
2025-06-09 02:49:27 +0000

    Responsivness initialized

--- src/Components/Order.jsx
+++ src/Components/Order.jsx
@@ -22,12 +22,12 @@ const Order = () => {
   };
 
   return (
-    <div className='h-fit w-screen bg-right bg-cover flex' 
+    <div className='h-fit w-screen bg-right bg-cover flex max-lg:flex-col' 
         style={{ backgroundImage: `url(${Bg})` }}	
     >
-      <div className='flex flex-col h-screen w-1/5 items-center justify-center'>
-        <div className=' w-full h-[10%] mb-auto shadow-2xl bg-white/10'></div>
-        <div className='flex flex-col h-[90%] w-full bg-white/10 items-center justify-center '>
+      <div className='flex flex-col h-screen max-lg:h-fit mb-5 w-1/5 items-center justify-center max-lg:w-full'>
+        <div className=' w-full h-[10%] mb-auto shadow-2xl bg-white/10 max-lg:hidden'></div>
+        <div className='flex flex-col h-[90%] w-full bg-white/10 items-center justify-center max-lg:text-center max-lg:h-[20%]'>
           <h1 className='mr-auto ml-[2vw] font-syne font-bold text-6xl text-white'>Name</h1>
           <h1 className='mr-auto ml-[2.3vw] font-syne font-thin text-xl text-white'>Id: 001010</h1>
         </div>
@@ -35,10 +35,10 @@ const Order = () => {
 
       <div className='flex flex-col h-screen w-4/5 '>
         <div className='w-full shadow-2xl h-[10%] bg-white/10 justify-center items-center text-start'>
-          <h1 className='mt-auto mb-auto font-syne font-bold text-6xl text-white'>Order Page</h1>
+          <h1 className='mt-auto mb-auto font-syne font-bold text-6xl max-lg:text-4xl text-white'>Order Page</h1>
         </div>
 
-        <div className='w-1/8 h-[7%] bg-white/10 rounded-xl  mb-auto mt-6 ml-3 flex gap-10 justify-center items-center '>
+        <div className='w-1/8 h-[7%] bg-white/10 rounded-xl mb-auto mt-6 ml-3 flex gap-10 justify-center items-center '>
           <button
             id='BillB'
             className='cursor-pointer w-fit h-fit flex flex-col justify-center items-center'
@@ -63,27 +63,27 @@ const Order = () => {
           <div className='h-[12vh] w-full '>
             <h1 className='font-syne font-bold text-7xl text-white'>Product-List</h1>
           </div>
-          <div className=''>
-            <div id='ids' className='w-full h-fit p-3 flex flex-row '>
+          <div className='w-full h-fit p-3 flex flex-row '>
+            <div id='ids' className='w-full h-fit p-3 flex flex-row max-lg:flex-col'>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>Code Product</p>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>Product</p>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>Price/Unit</p>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>Final Price</p>
             </div>
-          </div>
           <div id='info' className=''>
-            <div className='w-full h-fit p-3 flex flex-row '>
+            <div className='w-full h-fit p-3 flex flex-row max-lg:flex-col'>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>001</p>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>Product A</p>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>10.00</p>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>100.00</p>
             </div>
-            <div className='w-full h-fit p-3 flex flex-row '>
+            <div className='w-full h-fit p-3 flex flex-row max-lg:flex-col'>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>002</p>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>Product B</p>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>20.00</p>
               <p className='font-syne font-thin text-2xl flex-1 text-center  text-white'>200.00</p>
             </div>
+          </div>
           </div>
           <div className='flex mt-auto h-fit w-full p-3 '>
             <div className='mr-auto'>
